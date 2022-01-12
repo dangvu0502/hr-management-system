603198291281a08e65eb82a3ba58b10f20f28a03
G6 <Egamorft@DESKTOP-PKQO59B>
2022-01-12 22:01:51 +0700

    Add Connection and library

---
+++ HR Management/src/java/Context/DBContext.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Egamorft
+ */
+public class DBContext {
+    Connection conn;
+    public Connection getConnection() throws Exception{
+        String url = "jdbc:mysql://localhost:3306/hr_system";
+        String user = "root";
+        String password = "";
+        Connection conn = DriverManager.getConnection(url, user, password);
+        return conn;
+        
+    }
+    //check Connection
+//    public static void main(String[] args) {
+//        DBContext dBContext = new DBContext();
+//        try {
+//            System.out.println("Thanh Cong");
+//            System.out.println(dBContext.getConnection());
+//        } catch (Exception ex) {
+//            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+
+
+}
