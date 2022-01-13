17015d8151199cca3141f630e171de605963f325
dangGG <dangGG@LAPTOP-SNNCCLC0>
2022-01-13 16:28:42 +0700

    thêm user verify mail controller và user verify mail view

--- HR Management/src/java/Context/SendEmail.java
+++ HR Management/src/java/Context/SendEmail.java
@@ -30,57 +30,7 @@ public class SendEmail {
     }
 
     //send email to the user email
-    public boolean sendEmail(Account account) {
-        boolean test = false;
 
-        String toEmail = account.getEmail();
-        String fromEmail = "haidang0528@gmail.com";
-        String password = "H@idang0502";
-
-        try {
-
-            // your host email smtp server details
-            Properties pr = new Properties();
-            pr.setProperty("mail.smtp.host", "smtp.mail.com");
-            pr.setProperty("mail.smtp.port", "587");
-            pr.setProperty("mail.smtp.auth", "true");
-            pr.setProperty("mail.smtp.starttls.enable", "true");
-            pr.put("mail.smtp.socketFactory.port", "587");
-            pr.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
- 
-            //get session to authenticate the host email address and password
-            Session session = Session.getInstance(pr, new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(fromEmail, password);
-                }
-            });
-
-            //set email message details
-            Message mess = new MimeMessage(session);
-
-    		//set from email address
-            mess.setFrom(new InternetAddress(fromEmail));
-    		//set to email address or destination email address
-            mess.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
-    		
-    		//set email subject
-            mess.setSubject("User Email Verification");
-            
-    		//set message text
-            mess.setText("Registered successfully.Please verify your account using this code: " + account.getCode());
-            //send the message
-            Transport.send(mess);
-            
-            test=true;
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return test;
-    }
-    
     public boolean send(String to, String sub,
             String msg) {
         
