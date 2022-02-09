8af54d1f22019d2c7bd3ff0db662b92a21fc39ca
dangGG <dangvu0502@gmail.com>
2022-02-09 14:48:57 +0700

    update NewUserController

--- HR Management/src/java/Controller/NewUserController.java
+++ HR Management/src/java/Controller/NewUserController.java
@@ -30,14 +30,14 @@ public class NewUserController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        String action = request.getPathInfo() == null ? "" : request.getPathInfo();
-        //PrintWriter out = response.getWriter();
-        //out.println(action+" "+method);
-        try {
+        try (PrintWriter out = response.getWriter();) {
+            String action = request.getPathInfo() == null ? "" : request.getPathInfo();
+            String method = request.getMethod();
+            
             switch (action) {
-                
+
                 default:
-                    view(request, response);
+                    showNewUserView(request, response);
                     break;
             }
         } catch (Exception ex) {
@@ -45,47 +45,24 @@ public class NewUserController extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="Click on the + sign on the left to see details">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    
-    private void view(HttpServletRequest request, HttpServletResponse response)
+    private void showNewUserView(HttpServletRequest request, HttpServletResponse response)
             throws Exception {
         response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
         request.getRequestDispatcher("Views/NewUserView.jsp").forward(request, response);
     }
 
 // </editor-fold>
-
 }
