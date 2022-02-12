87ba870189a93c534521a13e26ded59c5ab31bb0
Hide <Hide@DESKTOP-M4KKLAE>
2022-02-13 00:03:05 +0700

    Settingdetail + supporttype

---
+++ HR Management/src/java/Controller/SettingController111.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DAO.SettingDAO;
+import Models.Setting;
+import java.io.IOException;
+import java.util.Vector;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Kha Chinh
+ */
+@WebServlet(name = "SettingController111", urlPatterns = {"/Setting1"})
+public class SettingController111 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try {
+            String page = request.getParameter("page");
+            if (page == null) {
+                page = "1";
+            }
+            request.setAttribute("page", page);
+            SettingDAO sDAO = new SettingDAO();
+            int count = sDAO.getTotalSetting();
+            int endPage = count / 5;
+            if (count % 5 != 0) {
+                endPage++;
+            }
+            request.setAttribute("endP", endPage);
+            Vector<Setting> s;
+            s = sDAO.getSettingList(Integer.parseInt(page));
+            request.setAttribute("listS", s);
+            request.getRequestDispatcher("Views/SettingList.jsp").forward(request, response);
+        } catch (Exception e) {
+            System.out.println("ádfasdfasdfasd" + e.getMessage());
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
