bfc5d7cbb3d86bec9fc202b6ffd07b5bf6162cc5
quocb <quocb@DESKTOP-BTRNF6K>
2022-02-21 15:45:44 +0700

    Them request

--- HR Management/src/java/Controllers/RequestController.java
+++ HR Management/src/java/Controllers/RequestController.java
@@ -5,6 +5,7 @@
  */
 package Controllers;
 
+import Models.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -32,17 +33,31 @@ public class RequestController extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RequestController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RequestController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try (PrintWriter out = response.getWriter();) {
+            String action = request.getPathInfo() == null ? "" : request.getPathInfo();
+            String method = request.getMethod();
+            switch (action) {
+                case "/RequestList":
+                    showRequestListView(request, response);
+                    break;
+                case "/NewTimesheet":
+                    newRequest(request, response, method);
+                    break;
+                case "/TimesheetDetail":
+                    showRequestDetailView(request, response);
+                    break;
+                case "/DeleteTimesheet":
+                    deleteRequest(request, response);
+                    break;
+                case "/EditTimesheet":
+                    editRequest(request, response, method);
+                    break;
+                default:
+                    response.sendError(404);
+                    break;
+            }
+        } catch (Exception ex) {
+            log(ex.getMessage());
         }
     }
 
@@ -75,14 +90,13 @@ public class RequestController extends HttpServlet {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    private void showRequestListView (HttpServletRequest request, HttpServletResponse response){
+        response.setContentType("text/html;charset=UTF-8");
+        User user = (User) request.getSession().getAttribute("account");
+        
+    }
+
+
+// </editor-fold>
 
 }
